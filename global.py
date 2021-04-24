62f44c6b75bc140365d32f96a0355470f58f4ebd
Noah Kenton <79272447+noahkenton@users.noreply.github.com>
2021-04-24 17:11:47 -0400

    Add files via upload

---
+++ global.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python3.9
+import os
+import argparse
+import plugin
+from plugin import MethodTshark
+import kismet_proc
+import subprocess
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--automated', '-a',action='store_true' , help='perform a run with default values')
+    parser.add_argument('--run', help='perform a normal run')
+    parser.add_argument('--read', '-r', help='read pcap file')
+
+    args = parser.parse_args()
+
+    output = MethodTshark(args.read, None, None, None).output_file()
+
+    if args.automated is True:
+        MethodTshark(args.read, output, None, None).filter()
+        subprocess.Popen(['kismet'])
+        kismet_proc.kismet(output, None, 'add-source')
+    elif args.run is not None:
+        plugin.main()
+        kismet_proc.main()
+
+
+if __name__ == '__main__':
+    main()
